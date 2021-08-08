cbf9cfa842801e70c70804b2c4230836433a2c82
Evellyn Lima <evellynlrl@hotmail.com>
2021-08-08 14:32:21 -0300

    Add support to sass. Add first component.

---
+++ src/components/Home/Top/index.tsx
@@ -0,0 +1,36 @@
+import {
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonIcon,
+  IonButton,
+} from "@ionic/react";
+import { close, helpCircleOutline } from "ionicons/icons";
+
+import "./styles.scss";
+
+const HomeTop: React.FC = () => {
+  return (
+    <div className="home__top">
+      <IonHeader collapse="condense" className="home__top-header ion-no-border">
+        <IonToolbar color="primary">
+          <IonButton slot="start" fill="clear">
+            <IonIcon icon={close} color="secondary"></IonIcon>
+          </IonButton>
+
+          <IonTitle size="small">Dinheiro guardado</IonTitle>
+
+          <IonButton slot="end" fill="clear">
+            <IonIcon icon={helpCircleOutline} color="secondary"></IonIcon>
+          </IonButton>
+        </IonToolbar>
+
+        <div className="home__top-value ion-text-center ion-padding-vertical">
+          <p>R$ 9.213,04</p>
+        </div>
+      </IonHeader>
+    </div>
+  );
+};
+
+export default HomeTop;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { IonContent, IonPage } from "@ionic/react";
+
+import Top from "../components/Home/Top";
+import "./Home.css";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <Top />
       </IonContent>
     </IonPage>
   );
