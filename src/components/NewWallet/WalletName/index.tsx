1dd41b4d94f592f990ef713bff0c9d2d7f79bec1
Evellyn Lima <evellynlrl@hotmail.com>
2021-08-09 10:07:55 -0300

    Add new component: wallet name

---
+++ src/components/NewWallet/WalletName/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.scss";
+
+const SelectIncome: React.FC = () => {
+  return (
+    <div className="wallet-name">
+      <p>Qual o nome do novo porquinho?</p>
+      <input />
+    </div>
+  );
+};
+
+export default SelectIncome;
--- src/pages/NewWallet.tsx
+++ src/pages/NewWallet.tsx
@@ -1,14 +1,14 @@
 import { IonContent, IonPage } from "@ionic/react";
 
-import SelectIncome from "../components/NewWallet/SelectIncome";
-// import Accounts from "../components/Home/Accounts";
-// import Footer from "../components/Home/Footer";
+// import SelectIncome from "../components/NewWallet/SelectIncome";
+import WalletName from "../components/NewWallet/WalletName";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <SelectIncome />
+        {/* <SelectIncome /> */}
+        <WalletName />
       </IonContent>
     </IonPage>
   );
